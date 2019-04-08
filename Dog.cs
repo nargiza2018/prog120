a4fe3b46162c007bbf3128a285288293c5e8b67a
nargiza2018 <36006761+nargiza2018@users.noreply.github.com>
2019-04-08 11:48:26 -0700

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogHomeworkProject
+{
+    class Dog
+    {
+        public double Weight { get; set; }
+        public string Color { get; set; }
+        public void Wag(int howManyWags)
+        {
+            for (int i = 0; i < howManyWags; i++)
+            {
+                Console.Write("wag ");
+            }
+            Console.WriteLine();
+        }
+        public void Bark(int howManyBarks)
+        {
+            for (int i = 0; i < howManyBarks; i++)
+            {
+                Console.Write("woof ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogHomeworkProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate a new dog object called dog1 
+            Dog dog1 = new Dog();
+
+            // instantiate a new dog object called dog2
+            Dog dog2 = new Dog();
+
+            // call the Wag method on object dog1, passing in a 4
+            dog1.Wag(4);
+
+            // call the Bark method on object dog2, passing in a 3
+            dog2.Bark(3); 
+            Console.ReadLine();
+        }
+    }
+}
